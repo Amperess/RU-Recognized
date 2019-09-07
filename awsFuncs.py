a30961ed1c42b5365aebc34089058b86f230c8ab
Amber Rawson <aarawson@gmail.com>
2019-09-07 19:49:04 -0400

    Integrated actors code to app.py

--- awsFuncs.py
+++ awsFuncs.py
@@ -1,7 +1,9 @@
 import boto3
-""" framePath: path to frame image
-    return: list of 3-tuples of the form (celebrity Name, Confidence, URL) 
-    obtain list of all the celebrities in the frame with the confidence level"""
+"""
+ framePath: path to frame image
+ return: list of 3-tuples of the form (celebrity Name, Confidence, URL)
+ obtain list of all the celebrities in the frame with the confidence level
+"""
 def getCelebsFromFrame(frame):
     client=boto3.client('rekognition')
     with open(frame, 'rb') as image:
@@ -19,4 +21,3 @@ def getCelebsFromFrame(frame):
     # screen: path to jpg screencap
     # return: string of text in the image (or a list if necessary)
     # """analyze frame and retrieve all relevant text from it"""
-
