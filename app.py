818e6fc3f1ff6f7bb37fc0300a98a3c35f205f07
Amber Rawson <aarawson@gmail.com>
2019-09-07 13:35:27 -0400

    Upload files done

--- app.py
+++ app.py
@@ -1,19 +1,50 @@
-]from flask import Flask, request
+import os
+from flask import Flask, flash, request, redirect, url_for
 import urllib
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = 'movies'
+ALLOWED_EXTENSIONS = {'mp4', "MP4"}
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('process_file',
+                                    filename=filename))
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
 
 
-# Home page route
-@app.route('/')
-def home():
-    # put home things here
-    return str(response)
-
-
-# A route to handle uploading/processing mp4
-@app.route('/upload', methods=['POST'])
-def upload_mp4():
+# A route to handle processing mp4
+@app.route('/upload', methods=['GET'])
+def process_file():
 
     # video = request.form['Body']
 
@@ -36,7 +67,7 @@ def upload_mp4():
 
     # finalGuesses = merge(guess1, guess2, guess3)
 
-    return str(response)
+    return "Hello World " + request.args.get('filename')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
