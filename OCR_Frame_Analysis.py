52beac982d0f4c0f86d2aabee840cbb589def1e1
Josh Siegel <joshuascottsoftware@gmail.com>
2019-09-07 21:13:34 -0400

    add pics and backup

---
+++ OCR_Frame_Analysis.py
@@ -0,0 +1,30 @@
+#Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#PDX-License-Identifier: MIT-0 (For details, see https://github.com/awsdocs/amazon-rekognition-developer-guide/blob/master/LICENSE-SAMPLECODE.)
+
+import boto3
+#import json
+import sys
+
+def OCRFrame(frame): 
+    client=boto3.client('rekognition')
+    with open(frame, 'rb') as image:
+        response = client.detect_text(Image={'Bytes': image.read()})
+    textDetections=response['TextDetections']
+    print ('Detected text')
+    combinedText = []
+    for text in textDetections:
+        print ('Detected text:' + text['DetectedText'])
+        print ('Confidence: ', text['Confidence'])
+        # print ('Id: {}'.format(text['Id']))
+        # if 'ParentId' in text:
+        #     print ('Parent Id: {}'.format(text['ParentId']))
+        print ('Type:' + text['Type'])
+        if text['Confidence'] > 85 and text['Type'] == 'WORD':
+            combinedText.append(text['DetectedText'])
+    return ' '.join(combinedText)
+
+if __name__ == "__main__":
+    framePath=sys.argv[1]
+    print(OCRFrame(framePath))
+        
+
